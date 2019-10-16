50b8981babef822e6a5f058c1ec8322b02eff842
Sangappa Rodda <sarodda@microsoft.com>
2019-10-16 16:20:18 +0530

    this is first setup and initial commit

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController: ApiController
+    {
+        public IEnumerable<User> Get() {
+            return new User[] {
+                new User{ Id=1,FirstName="Sangappa",LastName="Rodda" },
+                new User{ Id=1,FirstName="Sidhharth",LastName="Rodda" }
+            };
+        }
+
+       
+    }
+}
